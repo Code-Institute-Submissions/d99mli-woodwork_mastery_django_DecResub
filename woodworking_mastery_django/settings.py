61faf6383612e53513030c0bfdeba8023b998fde
d99mli <d99mli@gmail.com>
2021-12-01 11:54:27 +0000

    Heroku deployment debug

--- woodworking_mastery_django/settings.py
+++ woodworking_mastery_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k15@%18zimfpyaj=nw)gi=h7^@8twy8_a(wluk!zbnny1%m#!3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['woodworking-mastery-django.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -103,12 +103,17 @@ WSGI_APPLICATION = 'woodworking_mastery_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
